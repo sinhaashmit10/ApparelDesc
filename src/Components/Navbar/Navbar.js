caec026d4649fefbbdb842ee8b138a40eb284c71
Ashmit Sinha <sinhaashmit10@gmail.com>
2024-03-15 04:57:50 +0530

    commit

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -21,12 +21,12 @@ const Navbar = () => {
 
         <img src="menu.svg" alt="Menu" className='mobMenu' onClick={()=>setShowMenu(!showMenu)} />
         <div className="navMenu" style={{display: showMenu? 'flex':'none'}}>
-            <Link activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={700} className="listItem" onClick={()=>setShowMenu(false)}>Home</Link>
-            <Link activeClass='active' to='about' spy={true} smooth={true} offset={-55} duration={700} className="listItem" onClick={()=>setShowMenu(false)}>About</Link>
-            <Link activeClass='active' to='skills' spy={true} smooth={true} offset={-80} duration={700} className="listItem" onClick={()=>setShowMenu(false)}>Skills</Link>
-            <Link activeClass='active' to='portfolio' spy={true} smooth={true} offset={-90} duration={700} className="listItem" onClick={()=>setShowMenu(false)}>Portfolio</Link>
-            <Link activeClass='active' to='experience' spy={true} smooth={true} offset={-60} duration={700} className="listItem" onClick={()=>setShowMenu(false)}>Experience</Link>
-            <Link activeClass='active' to='contactMe' spy={true} smooth={true} offset={-60} duration={700} className="listItem" onClick={()=>setShowMenu(false)}>Contact</Link>
+            <Link activeClass='active' to='aboutTitle' spy={true} smooth={true} offset={-100} duration={700} className="listItem" onClick={()=>setShowMenu(false)}>Home</Link>
+            <Link activeClass='active' to='embroidery' spy={true} smooth={true} offset={-85} duration={700} className="listItem" onClick={()=>setShowMenu(false)}>Embroidery</Link>
+            <Link activeClass='active' to='materialCare' spy={true} smooth={true} offset={-80} duration={700} className="listItem" onClick={()=>setShowMenu(false)}>Material</Link>
+            {/* <Link activeClass='active' to='portfolio' spy={true} smooth={true} offset={-90} duration={700} className="listItem" onClick={()=>setShowMenu(false)}>Portfolio</Link> */}
+            {/* <Link activeClass='active' to='experience' spy={true} smooth={true} offset={-60} duration={700} className="listItem" onClick={()=>setShowMenu(false)}>Experience</Link> */}
+            {/* <Link activeClass='active' to='contactMe' spy={true} smooth={true} offset={-60} duration={700} className="listItem" onClick={()=>setShowMenu(false)}>Contact</Link> */}
         </div>
     </nav>
   )
